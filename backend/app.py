7f1cd57ddb4e61787a1b74c5b0a0203eaa4be089
Nann1207 <nirubakichor@gmail.com>
2026-03-13 21:52:10 +0800

    Created initial requirements txt file and test app.py

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.get("/health")
+def health():
+    return jsonify(status="hello")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
